688da4bd54c367c6f9aa4edb106103870675b0fe
Nathan Carter <ncarter@bentley.edu>
2019-02-26 13:26:51 +0000

    Fixing out-of-date parts of the documentation

--- lib/ch6limitations.gd
+++ lib/ch6limitations.gd
@@ -4,17 +4,24 @@
 #! When this package is being used in a Jupyter Notebook, it has the
 #! following limitations.
 #!
-#!  * If a notebook with visualizations created by this package is saved and
-#!    later reloaded, the visualizations will not persist.  They will be
-#!    replaced by an error message instructing the user to re-run the cell
-#!    that created the visualization.  This limitation may be removable with
-#!    some refactoring of the internal workings of this package.
+#!  * If this package is used in <Code>PlotDisplayMethod_Jupyter</Code>
+#!    mode in a Jupyter notebook, and visualizations are created by this
+#!    package, then the notebook is saved and later reloaded, the
+#!    visualizations will not persist.  They will be replaced by an error
+#!    message instructing the user to re-run the cell that created the
+#!    visualization.  You can get around this by setting
+#!    <Code>PlotDisplayMethod := PlotDisplayMethod_JupyterSimple</Code>,
+#!    but this increases the size of your notebook by embedding all the
+#!    JavaScript needed by the visualizations in the notebook itself.
+#!    Note that <Code>PlotDisplayMethod_Jupyter</Code> is the default
+#!    mode in the notebook.
 #!  * The <Code>nbconvert</Code> tool, which converts <File>.ipynb</File>
 #!    files into other formats, will not include the visualizations, because
 #!    <Code>nbconvert</Code> is not a browser that can evaluate the
 #!    JavaScript code that generates the visualizations.
-#!  * Since most visualizations load a JavaScript library from a CDN, they
-#!    require a working Internet connection to function.
+#!  * When using the <Code>PlotDisplayMethod_Jupyter</Code> mode, most
+#!    visualizations load a JavaScript library from a CDN, which thus
+#!    requires a working Internet connection to function.
 #!
 #! When it is being used from the command line, it has the following
 #! limitations.
